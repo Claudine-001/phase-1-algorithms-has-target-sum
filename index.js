ff574f714eff66f2e01070e3f255365c6b46d8e2
claudine-001 <claudine.kimani@student.moringaschool.com>
2022-12-12 19:11:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const viewedNo = {};
+
+  for (const no of array) {
+    // n steps
+    const complement = target - no;
+    if (complement in viewedNo) return true;
+    viewedNo[no] = true;
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
+function findSock(array) {
+  for (const item of array) {
+    if (item === "sock") return "sock";
+  }
+}
 
 /* 
   Add your pseudocode here
 */
+function findSock(object) {
+  if (object.sock) return "sock";
+}
 
 /*
   Add written explanation of your solution here
@@ -16,19 +34,9 @@ function hasTargetSum(array, target) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
+// @@ -31,4 +40,4 @@ if (require.main === module) {
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
+module.exports = hasTargetSum;
 module.exports = hasTargetSum;
